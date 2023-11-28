a8d4fe439904d1c5af171030d6885802a17308e3
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-11-28 15:11:21 -0300

    ParsingHTML and Portscan Powershell

---
+++ PortScanPS1.ps1
@@ -0,0 +1,14 @@
+﻿param($ip)
+clear
+if(!$ip){
+    echo "DESEC SECURITY - PORTSCAN"
+    echo "Modo de uso: .\PortScanPS1 192.168.0.1 80"
+}else{
+$topports = 21,22,25,80,443,3306
+try{foreach($porta in $topports){
+if(Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet){
+    echo "Porta $porta Aberta" 
+}}else {
+    echo "Porta $porta Fechada"
+ }}catch{}
+}
---
+++ parsingHTMLPS1.ps1
@@ -0,0 +1,13 @@
+﻿clear
+$site = Read-Host "Digite o site"
+$web = Invoke-WebRequest -uri "$site" -Method Options
+echo "O servidor roda:"
+$web.headers.server
+echo "====================="
+echo "O servidor aceita os métodos:"
+$web.Headers.Allow
+echo "====================="
+echo "Links encontrados:"
+$web2 = Invoke-WebRequest -uri "$site"
+$web2.Links.href | Select-String http://
+echo "====================="
